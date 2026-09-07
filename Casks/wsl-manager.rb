085bb3d5b537d1133678f6a07b2a8a8fe7770cb0
Eric Trenkel <eric@bostrot.com>
2026-09-07 23:59:30 +0200

    wsl-manager 2.0.1: rebuilt macOS binary (SIGPIPE fix)

--- Casks/wsl-manager.rb
+++ Casks/wsl-manager.rb
@@ -1,6 +1,6 @@
 cask "wsl-manager" do
   version "2.0.1"
-  sha256 "c628af3a6e7f16ff82436bdfb7cb5c8dcb2856ffce8f4748020339182d188100"
+  sha256 "d1f1c474c8b9f92fab2e0799ff9a597bdc7cbfc56b0decc8db484a401b7bc65c"
 
   url "https://github.com/bostrot/wsl2-distro-manager/releases/download/v#{version}/wsl2-distro-manager-v#{version}-macos.dmg"
   name "WSL Manager"
